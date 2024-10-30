05a74f24c99e5153df640ef25b445b358a732e32
Varun Repo <varunbali345@gmail.com>
2024-10-31 03:35:09 +0530

    home component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Linguistic theories and experiments by Rahul Bali'
+  title = 'Linguistic theory and experiments by Rahul Bali'
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { WordsComponent } from './component/words/words.component';
+import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
     // write route to words component with /words path
+    { path: '', component: HomeComponent },
     { path: 'words', component: WordsComponent },
 ];
