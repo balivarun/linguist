e0c0bd03d25c6f2b90474a8b722624eb31bd93ee
Rahul Bali <rahulrdb18@gmail.com>
2024-10-30 17:29:18 +0000

    removed extras styles and added words components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WordsComponent } from './component/words/words.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // write route to words component with /words path
+    { path: 'words', component: WordsComponent },
+];
